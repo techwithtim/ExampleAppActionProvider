f327b3b9acf2c192dd1c17d76b86852553715221
Tim <techwithtimm@gmail.com>
2025-08-26 20:08:36 +0400

    Add project files.

---
+++ ActionProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.AI.Actions;
+using Windows.AI.Actions.Provider;
+using Windows.Foundation;
+
+namespace ExampleAppActionProvider
+{
+    // AppActionProvider.cs
+    [System.Runtime.InteropServices.GuidAttribute("0067C3DD-920A-4486-83C3-9FAD82AFFA7A")]
+    public partial class AppActionProvider : IActionProvider {
+        public IAsyncAction InvokeAsync(ActionInvocationContext context)
+        {
+            return InvokeAsyncHelper(context).AsAsyncAction();
+        }
+
+        // AppActionProvider.cs
+        async Task InvokeAsyncHelper(ActionInvocationContext context)
+        {
+            NamedActionEntity[] inputs = context.GetInputEntities();
+
+            // Replace this line:
+            // var actionId = context.ActionId;
+            // With the following line to use ActionName instead:
+            var actionId = context.ActionId;
+            switch (actionId)
+            {
+                case "ExampleActionProvider.SendMessage":
+                    foreach (NamedActionEntity inputEntity in inputs)
+                    {
+                        if (inputEntity.Name.Equals("message", StringComparison.Ordinal))
+                        {
+
+                            TextActionEntity entity = (TextActionEntity)(inputEntity.Entity);
+                            string message = entity.Text;
+
+                            // TODO: Process the message and generate a response
+
+                            string response = "This is the message response";
+                            TextActionEntity result = context.EntityFactory.CreateTextEntity(response);
+                            context.SetOutputEntity("response", result);
+
+                        }
+
+                    }
+
+                    break;
+
+                default:
+
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Program.cs
+
+using ExampleAppActionProvider;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+[DllImport("ole32.dll")]
+
+static extern int CoRegisterClassObject(
+            [MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+            [MarshalAs(UnmanagedType.IUnknown)] object pUnk,
+            uint dwClsContext,
+            uint flags,
+            out uint lpdwRegister);
+
+[DllImport("ole32.dll")] static extern int CoRevokeClassObject(uint dwRegister);
+
+uint cookie;
+
+Guid CLSID_Factory = Guid.Parse("0067C3DD-920A-4486-83C3-9FAD82AFFA7A");
+CoRegisterClassObject(CLSID_Factory, new COM.WidgetProviderFactory<AppActionProvider>(), 0x4, 0x1, out cookie);
+
+Application.Start((p) =>
+{
+    var context = new DispatcherQueueSynchronizationContext(
+        DispatcherQueue.GetForCurrentThread());
+    SynchronizationContext.SetSynchronizationContext(context);
+    _ = new App();
+});
+
+CoRevokeClassObject(cookie);
+
+return 0;
